7e045d06a7ac74320af29d2d709e2c35fa0cbfa7
poonamdodake <poonamdodake@gmail.com>
2022-03-23 14:50:24 +0530

    file added

---
+++ SpringBootCoreApp/src/main/java/com/csi/controller/ApplicationController.java
@@ -0,0 +1,28 @@
+package com.csi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+
+public class ApplicationController {
+
+    @GetMapping
+    public String sayWelcome()
+    {
+        return "Welcome to Finetech to csi";
+    }
+    @GetMapping("/service")
+    public String sayService()
+    {
+        return "software development | QR code";
+    }
+    @GetMapping("/address")
+    public String address()
+    {
+        return "Pune";
+    }
+
+}
